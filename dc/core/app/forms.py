573a7789fe99a8ee113dca1ddd7e86723c78a629
Joshua Ntongai <jntongai@vdi.konza>
2022-06-13 11:35:21 +0300

    First commit for services with ugettext_lazy as gettext_lazy and django 4.0.5

--- dc/core/app/forms.py
+++ dc/core/app/forms.py
@@ -1,6 +1,6 @@
 from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from .models import ContactUs
 
 class ContactForm(forms.ModelForm):
